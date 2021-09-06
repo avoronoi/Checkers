6300b35f91a222a47d7e0ad63c5c8019a4ac3f23
Andrey <andrey.voronoi@gmail.com>
2021-09-06 21:28:31 +0300

    -fixed a bug leading to disappearing of a piece -implemented yellow coloring for a selected piece

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import tkinter as tk
 import logic
 
+
 # 0 - empty, 1 - white, 2 - black
 BOARD_NUM = [
-    [0, 0, 0, 2, 0, 2, 0, 2],
-    [2, 0, 1, 0, 2, 0, 2, 0],
-    [0, 2, 0, 0, 0, 2, 0, 2],
+    [0, 2, 0, 2, 0, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 2, 0, 2, 0, 2, 0, 2],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 0, 0, 0, 0, 1, 0],
-    [0, 1, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0]
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0]
 ]
 logic.init_board(BOARD_NUM)
+
 
 BOARD_SIZE = len(BOARD_NUM)
 CELL_SIZE = 80
 WHITE_CELL = '#ececd0'
 BLACK_CELL = '#779557'
+SELECTED_CELL = '#Bab867'
 # the first value is for a non-captured piece, the second value is for captured
 WHITE_PIECE = ('#f9f9f9', '#CAD5BE')
 BLACK_PIECE = ('#595451', '#697754')
@@ -26,13 +29,15 @@ BLACK_OUTLINE = '#3a3a31'
 WHITE_CELL_MOVE = '#d6d6bd'
 BLACK_CELL_MOVE = '#69874e'
 
+
 selected_moves = []
 selected_cell = None
 # 1 if white has to make the move else 2
-turn = 2
+turn = 1
 captured_pieces = []
 # None if the move is over otherwise the coordinates of a cell to continue the move
 piece_to_continue = None
+
 
 class CnvCell(tk.Canvas):
     def __init__(self, piece, row, col, **kwargs):
@@ -73,7 +78,8 @@ class FrmBoard(tk.Frame):
                 cnv_cell = CnvCell(
                     logic.board[i][j], i, j, master=self,
                     width=CELL_SIZE, height=CELL_SIZE, highlightthickness=0,
-                    bg=WHITE_CELL if (i + j) % 2 == 0 else BLACK_CELL)
+                    bg=(SELECTED_CELL if selected_cell == (i, j) 
+                        else WHITE_CELL if (i + j) % 2 == 0 else BLACK_CELL))
                 cnv_cell.bind('<Button-1>', click_cell)
                 cnv_cell.place(x=j*CELL_SIZE, y=i*CELL_SIZE)
 
@@ -102,15 +108,18 @@ def move(row_from, col_from, row_to, col_to):
     captured_piece, change_turn = logic.board[row_from][col_from].move_to(row_to, col_to)
     global turn
     global piece_to_continue
+    global captured_pieces
     if captured_piece is not None:
         captured_pieces.append(captured_piece)
         redraw()
+    print(captured_pieces)
     if change_turn:
         for x, y in captured_pieces:
             logic.board[x][y] = None
         redraw()
         turn = 3 - turn
         piece_to_continue = None
+        captured_pieces = []
     else:
         piece_to_continue = (row_to, col_to)
         select(row_to, col_to)
